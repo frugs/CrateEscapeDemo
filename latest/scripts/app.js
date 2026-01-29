982038e3f595f347d8f484c1417954876ae9696d
Hugo Wainwright <1161180+frugs@users.noreply.github.com>
2026-01-29 23:43:18 +0000

    v0.0.0.21

--- latest/scripts/app.js
+++ latest/scripts/app.js
@@ -27,7 +27,7 @@ function initializeGame() {
         streamingAssetsUrl: "StreamingAssets",
         companyName: "EkHypnon",
         productName: "Crate Escape",
-        productVersion: "0.0.0.16",
+        productVersion: "0.0.0.21",
     }).then((unityInstance) => {
         console.log("Unity instance created successfully");
 
